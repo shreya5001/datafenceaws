2d9569d130cad4a056afe498554eda4ddc125f09
Manoj Kukreja <mkukreja1@gmail.com>
2023-02-14 12:14:54 -0500

    emr

--- emr/wordcount.py
+++ emr/wordcount.py
@@ -1,22 +1,17 @@
 import sys
-from operator import add
 
-from pyspark.sql import SparkSession
-
+from pyspark import SparkContext, SparkConf
 
 if __name__ == "__main__":
 
-    spark = SparkSession\
-        .builder\
-        .appName("PythonWordCount")\
-        .getOrCreate()
+        # create Spark context with necessary configuration
+        sc = SparkContext("local","PySpark Word Count Exmaple")
 
-    lines = spark.read.text("s3://aws-analytics-course/temp/const.txt").rdd.map(lambda r: r[0])
-    counts = lines.flatMap(lambda x: x.split(' ')) \
-                  .map(lambda x: (x, 1)) \
-                  .reduceByKey(add)
-    output = counts.collect()
-    for (word, count) in output:
-        print("%s: %i" % (word, count))
+        # read data from text file and split each line into words
+        words = sc.textFile("s3://aws-analytics-course/temp/const.txt").flatMap(lambda line: line.split(" "))
 
-    spark.stop()
+        # count the occurrence of each word
+        wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
+
+        # save the counts to output
+        wordCounts.saveAsTextFile("s3://aws-analytics-course/temp/results")
