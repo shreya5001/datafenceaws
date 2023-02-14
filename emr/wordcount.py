9f0f257c2fef8043fb243ca7be71b59c99d65301
Manoj Kukreja <mkukreja1@gmail.com>
2023-02-14 13:02:03 -0500

    emr

--- emr/wordcount.py
+++ emr/wordcount.py
@@ -1,10 +1,15 @@
 import sys
-
 from pyspark import SparkContext, SparkConf
+from operator import add
+
+conf = (SparkConf()
+         .setMaster("local")
+         .setAppName("WordCounter")
+         .set("spark.executor.memory", "1g"))
+sc = SparkContext(conf = conf)
 
 if __name__ == "__main__":
     try:
-        sc = SparkContext("local","PySpark Word Count Exmaple")
         words = sc.textFile("s3://aws-analytics-course/temp/const.txt").flatMap(lambda line: line.split(" "))
         wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
         wordCounts.saveAsTextFile("s3://aws-analytics-course/temp/results")
